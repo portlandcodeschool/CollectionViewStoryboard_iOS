2c8eedb2172790505b5f804ca95c129f969ec656
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2014-12-09 17:41:32 -0800

    Initial Commit

---
+++ CollectionViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewExample
+//
+//  Created by Erick Bennett on 12/9/14.
+//  Copyright (c) 2014 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
